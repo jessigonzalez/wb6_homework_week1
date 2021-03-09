1cd7ab1127811b3742466b035d64117a268bbd08
jessigonzalez <31708279+jessigonzalez@users.noreply.github.com>
2021-03-09 17:46:13 -0500

    Improved solution
    Requires no additional space

--- sort_array_by_parity.py
+++ sort_array_by_parity.py
@@ -1,3 +1,13 @@
-class Solution:
+\class Solution:
     def sortArrayByParity(self, A: List[int]) -> List[int]:
-        return ([x for x in A if x%2==0] + [x for x in A if x%2==1])
+        start = 0 
+        end = len(A) - 1
+        while start < end:
+            if A[start] % 2 > A[end] % 2:
+                A[start], A[end] = A[end], A[start] #switch occurs here 
+            if A[start] % 2 == 0: #increment start value if even
+                start += 1
+            if A[end] % 2 == 1: #decrement end value if odd
+                end -= 1
+
+        return A
