a0eed96af2c4018d9725ace6964583bc0cf3d33e
jessigonzalez <31708279+jessigonzalez@users.noreply.github.com>
2021-03-09 20:54:31 -0500

    Update flipping_an_image.py

--- flipping_an_image.py
+++ flipping_an_image.py
@@ -4,14 +4,4 @@ class Solution:
         for row in image:
             flipped.append(row[::-1])
         
-        resultImage = []
-        for row in flipped:
-            curr = []
-            for element in row:
-                if element == 1:
-                    curr.append(0)
-                else:
-                    curr.append(1)
-            resultImage.append(curr)
-        return resultImage
-        
+        return[[0 if x==1 else 1 for x in x] for x in flipped] 
